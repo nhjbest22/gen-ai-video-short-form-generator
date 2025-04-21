8747fa0ed947292f2827ca6d6b37bd13015e52fa
nhjbest22 <nhjbest22@g.skku.edu>
2025-04-21 14:00:30 +0900

    feat: increase lambda memory to 1024MB

--- amplify/custom/ExtractTimeframe/resource.ts
+++ amplify/custom/ExtractTimeframe/resource.ts
@@ -1,13 +1,13 @@
-import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
-import { Construct } from 'constructs';
-import { IBucket } from 'aws-cdk-lib/aws-s3';
-import { ITable } from 'aws-cdk-lib/aws-dynamodb';
-import { Duration } from 'aws-cdk-lib/core';
-import { Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
+import { Construct } from "constructs";
+import { IBucket } from "aws-cdk-lib/aws-s3";
+import { ITable } from "aws-cdk-lib/aws-dynamodb";
+import { Duration } from "aws-cdk-lib/core";
+import { Effect, PolicyStatement } from "aws-cdk-lib/aws-iam";
 
 type ExtractTimeframeProps = {
-  bucket: IBucket,
-  highlightTable: ITable,
+  bucket: IBucket;
+  highlightTable: ITable;
 };
 
 export class ExtractTimeframe extends Construct {
@@ -16,15 +16,16 @@ export class ExtractTimeframe extends Construct {
     super(scope, id);
 
     // cdk consturct to create lambda function
-    this.handler = new Function(this, 'ExtractTimeframe', {
+    this.handler = new Function(this, "ExtractTimeframe", {
       runtime: Runtime.PYTHON_3_12,
-      code: Code.fromAsset('amplify/custom/lambda-functions/extract-timeframe'),
-      handler: 'lambda_function.lambda_handler',
+      code: Code.fromAsset("amplify/custom/lambda-functions/extract-timeframe"),
+      handler: "lambda_function.lambda_handler",
       environment: {
         BUCKET_NAME: props.bucket.bucketName,
         HIGHLIGHT_TABLE_NAME: props.highlightTable.tableName,
       },
       timeout: Duration.seconds(600),
+      memorySize: 1024,
     });
 
     props.bucket.grantReadWrite(this.handler);
@@ -33,11 +34,8 @@ export class ExtractTimeframe extends Construct {
       new PolicyStatement({
         effect: Effect.ALLOW,
         resources: ["*"],
-        actions: [
-          "mediaconvert:*",
-        ],
+        actions: ["mediaconvert:*"],
       })
-    )
-  
+    );
   }
 }
