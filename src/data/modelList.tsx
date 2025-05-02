030da3f1ba9393bcbd38aa3877560f3e8eb1ab4d
nhjbest22 <nhjbest22@g.skku.edu>
2025-05-03 01:32:09 +0900

    fix: add Claude 3.7 model

--- src/data/modelList.tsx
+++ src/data/modelList.tsx
@@ -9,35 +9,41 @@ export interface ModelOption {
 
 export const modelOptions: ModelOption[] = [
   {
-     name: "Claude 3.0 Opus",
-     logo: "logos/anthropic-logo.png",
-     modelId: "anthropic.claude-3-opus-20240229-v1:0",
-     provider: "Anthropic"
+    name: "Claude 3.0 Opus",
+    logo: "logos/anthropic-logo.png",
+    modelId: "anthropic.claude-3-opus-20240229-v1:0",
+    provider: "Anthropic",
   },
   {
     name: "Claude 3.5 Sonnet v1",
     logo: "logos/anthropic-logo.png",
     modelId: "anthropic.claude-3-5-sonnet-20240620-v1:0",
-    provider: "Anthropic"
+    provider: "Anthropic",
   },
   {
     name: "Claude 3.5 Sonnet v2",
     logo: "logos/anthropic-logo.png",
     modelId: "anthropic.claude-3-5-sonnet-20241022-v2:0",
-    provider: "Anthropic"
+    provider: "Anthropic",
+  },
+  {
+    name: "Claude 3.7 Sonnet",
+    logo: "logos/anthropic-logo.png",
+    modelId: "anthropic.claude-3-7-sonnet-20250219-v1:0",
+    provider: "Anthropic",
   },
   {
     name: "Claude 3.0 Sonnet",
     logo: "logos/anthropic-logo.png",
     modelId: "anthropic.claude-3-sonnet-20240229-v1:0",
-    provider: "Anthropic"
+    provider: "Anthropic",
   },
   {
     name: "Claude 3.0 Haiku",
     logo: "logos/anthropic-logo.png",
     modelId: "anthropic.claude-3-haiku-20240307-v1:0",
-    provider: "Anthropic"
-  }
+    provider: "Anthropic",
+  },
   // {
   //   name: "Llama 3 70b Instruct",
   //   logo: "logos/meta-logo.png",
